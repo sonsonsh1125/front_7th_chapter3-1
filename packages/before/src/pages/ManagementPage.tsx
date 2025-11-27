7d7868344918377ac974d635a5c41be7ea935ca1
sonsonsh1125 <tmdgus8911@gmail.com>
2025-11-27 22:45:56 +0900

    build: 배포관련 오류 수정

--- packages/before/src/pages/ManagementPage.tsx
+++ packages/before/src/pages/ManagementPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Badge } from '../components/atoms';
+import { Button } from '../components/atoms';
 import { Alert, Table, Modal } from '../components/organisms';
 import { FormInput, FormSelect, FormTextarea } from '../components/molecules';
 import { userService } from '../services/userService';
