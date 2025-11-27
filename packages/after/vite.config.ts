a647db7003b5875a216d75fae52bdd1069cba0be
sonsonsh1125 <tmdgus8911@gmail.com>
2025-11-27 22:43:51 +0900

    build: 배포관련 deploy 설정

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -5,18 +5,21 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // @ts-expect-error - vitest types are extended via reference directive
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
     css: true,
   },
-});
+  base: mode === "production" ? "/front_7th_chapter3-1/" : "/",
+  build: {
+    outDir: "dist", // vite 기본 값이 `dist` 이므로 생략 가능
+  },
+}));
